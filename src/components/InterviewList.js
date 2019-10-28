62df29a6276259322ece308fbb8b42582000ccb1
curriecode <melaniecurrie01@gmail.com>
2019-10-28 16:07:07 -0700

    added: interviewerList

--- src/components/InterviewList.js
+++ src/components/InterviewList.js
@@ -1,1 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "./InterviewerList.scss"
+
+export default function InterviewerList(props) {
+  const interviews = props.interviewers.map(mentor => {
+    return (
+      <InterviewerListItem
+        key={mentor.id}
+        name={mentor.name}
+        avatar={mentor.avatar}
+        selected={mentor.id === props.interviewer}
+        setInterviewer={props.setInterviewer}
+      />
+
+    );
+  })
+  return <Fragment>
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviews}
+      </ul>
+    </section>
+  </Fragment>
+}
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -21,12 +21,3 @@ export default function InterviewerListItem(props) {
     </li>
   )
 }
-
-// InterviewerListItem
-// Our InterviewerListItem takes in the following props:
-
-// id: number - the id of the interviewer
-// name: string - the name of the interviewer
-// avatar: url - a url to an image of the interviewer
-// selected: boolean - to determine if an interview is selected or not
-// setInterviewer: function - sets the interviewer upon selection
