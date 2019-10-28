42064ccf5eb3d2031b3f6bdc5197f93182ac1e55
curriecode <melaniecurrie01@gmail.com>
2019-10-28 13:38:16 -0700

    tests passing for 'spots remaing'

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,11 +8,23 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   })
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return 'no spots remaining'
+    }
+    if (props.spots === 2) {
+      return '2 spots remaining'
+    }
+    if (props.spots === 1) {
+      return '1 spot remaining'
+    }
+  }
   return (
-    <li className={dayClass}
+    <li
+      className={dayClass}
       onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
