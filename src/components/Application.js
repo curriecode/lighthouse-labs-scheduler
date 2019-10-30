d56deb1962ac709724bb64817d34ff3047afcc4e
curriecode <melaniecurrie01@gmail.com>
2019-10-29 19:00:37 -0700

    Update: getting days/ appt from api now

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,13 +4,14 @@ import axios from "axios"
 
 import "components/Application.scss";
 import DayList from "components/DayList"
+import getAppointmentsForDay from "helpers/selectors"
 import Appointment from "components/Appointment"
 
 const appointments = [
-  // {
-  //   id: 1,
-  //   time: "12pm",
-  // },
+  {
+    id: 1,
+    time: "12pm",
+  },
   {
     id: 2,
     time: "1pm",
@@ -62,16 +63,41 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([])
+  const setDay = day => setState({ ...state, day });
+  // const setDays = days => setState(prev => ({ ...prev, days }));
+
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+
+
+
 
 
   useEffect(() => {
-    axios.get("http://localhost:8001/api/days")
-      .then((response) => {
-        console.log(response.data);
-        setDays(response.data)
-      }, []);
+
+
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
+
+    ])
+      .then((res) => {
+        console.log(res[0].data)
+        setState(prev =>
+          ({
+            ...prev,
+            days: res[0].data,
+            appointments: res[1].data
+
+          }));
+      });
+
   })
 
   const apptList = appointments.map(appointment => {
@@ -92,7 +118,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
 
         </nav>
         <img
